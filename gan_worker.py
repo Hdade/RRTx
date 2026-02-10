7074f42331ff73fb801115c6a25bae4c8b294f60
Hdade <anhhoang001407@gmail.com>
2026-02-10 20:49:46 +0700

    FEAT: thêm chế độ chạy trên GPU

--- gan_worker.py
+++ gan_worker.py
@@ -4,15 +4,23 @@ import cv2
 import traceback
 import sys
 import os
+import torch
 
 sys.path.append(os.getcwd())
 from ViT_GAN.inferenceHelper import GANInference
 
 def gan_worker_loop(input_queue, output_queue, checkpoint_path, config_screen_dims):
-    print("[GAN Worker] Process started. Initializing model on CPU...")
+    #Tự động chọn thiết bị: CUDA (Nvidia) > MPS (Mac M1/M2) > CPU
+    device = 'cpu'
+    if torch.cuda.is_available():
+        device = 'cuda'
+    elif torch.backends.mps.is_available():
+        device = 'mps'
+        
+    print(f"[GAN Worker] Process started. Initializing model on {device}...")
     
     try:
-        gan_model = GANInference(checkpoint_path, device='cpu') 
+        gan_model = GANInference(checkpoint_path, device=device) 
         print(f"[GAN Worker] Model loaded successfully from {checkpoint_path}!")
 
         w, h = config_screen_dims
